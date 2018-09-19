c7f828890176fd92d32e7162f06ca079749774a8
Jeni Brandt <jenibrandt@legalshieldcorp.com>
2018-09-19 16:26:22 -0500

    completed new/create function

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -8,7 +8,24 @@ class WikisController < ApplicationController
   end
 
   def new
+    @wiki = Wiki.new
   end
+
+  def create
+    @wiki = Wiki.new
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+    @wiki.private = params[:wiki][:private]
+    @wiki.user = current_user
+
+    if @wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error saving the wiki. Please try again."
+      render :new
+    end
+  end 
 
   def edit
   end
