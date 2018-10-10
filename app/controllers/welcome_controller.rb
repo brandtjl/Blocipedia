9cbce40c86ef17cdeacb08f55a992437e73e2f74
Jeni Brandt <jenibrandt@legalshieldcorp.com>
2018-10-10 16:38:17 -0500

    completed Account Upgrade functionality

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,16 @@ class WelcomeController < ApplicationController
   end
 
   def about
+    if current_user.account == 'free'
+      flash[:notice] = "You are already on the Free Plan!"
+      redirect_to root_path
+    else
+      @user = User.find(current_user.id)
+      @user.account = 'free'
+      if @user.save
+        flash[:notice] = "You have been downgraded to the Free Plan!"
+        redirect_to wikis_path # or wherever
+      end
+    end 
   end
 end
