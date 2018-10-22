9ff692ffca4dd6adb14cf2096ae565546f0b2641
Jeni Brandt <jenibrandt@legalshieldcorp.com>
2018-10-22 08:16:01 -0500

    having trouble with routes

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -18,5 +18,14 @@ class CollaboratorsController < ApplicationController
   end
 
   def destroy
+    @collaborator = Collaborator.find(params[:id])
+
+    if @collaborator.destroy
+      flash[:notice] = "\"#{@collaborator.user_id}\" was deleted successfully."
+      redirect_to wikis_path
+    else
+      flash.now[:alert] = "There was an error deleting the collaborator."
+      render :show
+    end 
   end
 end
